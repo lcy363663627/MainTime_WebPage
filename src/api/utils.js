0e4271d20c5e59640175ca80ab6d68c69ab6d7c4
lcysnail <liuchengyingyx@gmail.com>
2017-09-22 17:42:00 +0800

    轮播图修改

--- src/api/utils.js
+++ src/api/utils.js
@@ -1,29 +1,63 @@
 var Util = {
-    getData: function () {
+    getData: function() {
 
     },
-    getElements: function (cls) {
-        var ele = document.getElementsByClassName(cls);
+    getElements: function(cls) {
+        var ele;
+        if (/\./.test(cls)) {
+            cls = cls.replace(/\./, '');
+            ele = document.getElementsByClassName(cls);
+            if (ele == null) {
+                throw new Error('--------------没有找到CLASS为' + cls + '的元素--------------!');
+            }
+        } else if (/\#/.test(cls)) {
+            cls = cls.replace(/\#/, '');
+            ele = document.getElementById(cls);
+            if (ele == null) {
+                throw new Error('--------------没有找到ID为' + cls + '的元素--------------!');
+            }
+        } else {
+            throw new Error('--------------参数格式错误-------------!');
+        }
+
         return ele;
     },
-    creatXML: function () {
+    creatXML: function() {
         var xml = null;
         if (window.XMLHttpRequest) {
             xml = new XMLHttpRequest();
-        }else if (window.ActiveXObject) {
+        } else if (window.ActiveXObject) {
             xml = new ActiveXObject("Microsoft.XMLHTTP");
         }
         return xml;
     },
-    setElements: function (data, obj) {
+    setElements: function(data, obj) {
         var title = obj.title;
         for (var i = 0; i < obj.ele.length; i++) {
-                obj.img[i].src = (function (a) {
-                    return data[a].img ? data[a].img : data[a].image;
-                })(i);
-                obj.p[i].innerHTML = (function (a) {
-                    return data[a][title];
-                })(i);
+            obj.img[i].src = (function(a) {
+                return data[a].img ? data[a].img : data[a].image;
+            })(i);
+            obj.p[i].innerHTML = (function(a) {
+                return data[a][title];
+            })(i);
+        }
+    },
+    addEvent: function(ele, type, fn) {
+        if (window.addEventListener) {
+            ele.addEventListener(type, fn);
+        } else if (window.attachEvent) {
+            ele.attachEvent('on' + type, fn);
+        } else {
+            ele["on" + type] = fn;
+        }
+    },
+    removeEvent: function(ele, type, fn) {
+        if (window.removeEventListener) {
+            ele.removeEventListener(type, fn);
+        } else if (window.attachEvent) {
+            ele.detachEvent('on' + type, fn);
+        } else {
+            ele["on" + type] = null;
         }
     }
 
--- src/js/home.js
+++ src/js/home.js
@@ -1,35 +1,79 @@
-
 var ele1 = {
-    ele: Util.getElements('div-img1'),
-    img: Util.getElements('img1'),
-    p: Util.getElements('name1'),
-    button: Util.getElements('li-img-btn'),
+    ele: Util.getElements('.div-img1'),
+    img: Util.getElements('.img1'),
+    p: Util.getElements('.name1'),
+    button: Util.getElements('.li-img-btn'),
     title: 'titleCn'
 }
 var ele2 = {
-    ele: Util.getElements('div-img2'),
-    img: Util.getElements('img2'),
-    p: Util.getElements('name2'),
-    button: Util.getElements('li-img-btn2'),
+    ele: Util.getElements('.div-img2'),
+    img: Util.getElements('.img2'),
+    p: Util.getElements('.name2'),
+    button: Util.getElements('.li-img-btn2'),
     title: 'tCn'
 }
 var ele3 = {
-    ele: Util.getElements('div-img3'),
-    img: Util.getElements('img3'),
-    p: Util.getElements('name3'),
-    button: Util.getElements('li-img-btn3'),
+    ele: Util.getElements('.div-img3'),
+    img: Util.getElements('.img3'),
+    p: Util.getElements('.name3'),
+    button: Util.getElements('.li-img-btn3'),
     title: 'title'
 }
+
 var that = this;
 var getData = http.getrequest('get', 'http://localhost:3000/PageSubArea/HotPlayMovies.api?locationId=290')
-    .then(function (res) {
+    .then(function(res) {
         Util.setElements(res.movies, that.ele1);
     });
 var getDataHot = http.getrequest('get', 'http://localhost:3000/Showtime/LocationMovies.api?locationId=290')
-    .then(function (res) {
+    .then(function(res) {
         Util.setElements(res.ms, that.ele2);
     });
 var getDataHot = http.getrequest('get', 'http://localhost:3000/Movie/MovieComingNew.api?locationId=290')
-    .then(function (res) {
+    .then(function(res) {
         Util.setElements(res.moviecomings, that.ele3);
     });
+
+
+
+var swiper_li = Util.getElements('.swiper-mian-div')[0];
+var swiper_img = Util.getElements('.sw-img1');
+var swiper_btn_left = Util.getElements('.swiper-btn-left')[0];
+var swiper_btn_right = Util.getElements('.swiper-btn-right')[0];
+
+Util.addEvent(swiper_btn_left, 'click', function(e) {
+    my_Animate(swiper_li, 'right', 1200);
+    // var parent = swiper_img[swiper_li.length-1].parentNode;
+    console.log(swiper_img[swiper_img.length - 1]);
+});
+Util.addEvent(swiper_btn_right, 'click', function(e) {
+    my_Animate(swiper_li, 'left', 1200);
+});
+
+function my_Animate(ele, direction, transitionX) {
+    var liftX = 0;
+    if (direction == 'left') {
+        leftX = ele.offsetLeft - 100;
+    } else {
+        leftX = ele.offsetLeft + 100;
+    }
+
+    function move() {
+        var timer = setTimeout(function() {
+            move();
+        }, 1);
+        if (Math.abs(leftX) % transitionX == 0) {
+            clearTimeout(timer);
+        }
+        if (direction == 'left') {
+            ele.style.left = leftX + "px";
+            leftX = leftX - 10;
+        } else if (direction == 'right') {
+            ele.style.left = leftX + "px";
+            leftX = leftX + 10;
+        }
+
+
+    }
+    move();
+}
