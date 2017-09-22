d60f88130f39f298e1bdcf0f882b2e69897c21b1
lcy363663627 <363663627@qq.com>
2017-09-22 23:59:31 +0800

    完善 轮播图

--- src/js/home.js
+++ src/js/home.js
@@ -21,15 +21,15 @@ var ele3 = {
 }
 
 var that = this;
-var getData = http.getrequest('get', 'http://localhost:3000/PageSubArea/HotPlayMovies.api?locationId=290')
+http.getrequest('get', 'http://localhost:3000/PageSubArea/HotPlayMovies.api?locationId=290')
     .then(function(res) {
         Util.setElements(res.movies, that.ele1);
     });
-var getDataHot = http.getrequest('get', 'http://localhost:3000/Showtime/LocationMovies.api?locationId=290')
+http.getrequest('get', 'http://localhost:3000/Showtime/LocationMovies.api?locationId=290')
     .then(function(res) {
         Util.setElements(res.ms, that.ele2);
     });
-var getDataHot = http.getrequest('get', 'http://localhost:3000/Movie/MovieComingNew.api?locationId=290')
+http.getrequest('get', 'http://localhost:3000/Movie/MovieComingNew.api?locationId=290')
     .then(function(res) {
         Util.setElements(res.moviecomings, that.ele3);
     });
@@ -40,6 +40,7 @@ var swiper_li = Util.getElements('.swiper-mian-div')[0];
 var swiper_img = Util.getElements('.sw-img1');
 var swiper_btn_left = Util.getElements('.swiper-btn-left')[0];
 var swiper_btn_right = Util.getElements('.swiper-btn-right')[0];
+var timer1;
 
 Util.addEvent(swiper_btn_left, 'click', function(e) {
     my_Animate(swiper_li, 'right', 1200);
@@ -49,30 +50,49 @@ Util.addEvent(swiper_btn_right, 'click', function(e) {
     my_Animate(swiper_li, 'left', 1200);
 });
 
+Util.addEvent(swiper_li, 'mouseenter', function (e) {
+    clearInterval(timer1);
+});
+
+Util.addEvent(swiper_li, 'mouseleave', function (e) {
+    timer1 = setInterval(function() {
+        my_Animate(swiper_li, 'left', 1200);
+    },5000);
+});
+
+timer1 = setInterval(function() {
+    my_Animate(swiper_li, 'left', 1200);
+},5000);
+
 function my_Animate(ele, direction, transitionX) {
     var liftX = 0;
     if (direction == 'left') {
-        leftX = ele.offsetLeft - 100;
+        leftX = ele.offsetLeft - 10;
     } else {
-        leftX = ele.offsetLeft + 100;
+        leftX = ele.offsetLeft + 10;
     }
-
     function move() {
         var timer = setTimeout(function() {
             move();
         }, 1);
+        
         if (Math.abs(leftX) % transitionX == 0) {
             clearTimeout(timer);
         }
         if (direction == 'left') {
             ele.style.left = leftX + "px";
             leftX = leftX - 10;
+            if (leftX <= -6000) {
+                ele.style.left = 0 + "px";
+            }
         } else if (direction == 'right') {
             ele.style.left = leftX + "px";
             leftX = leftX + 10;
+            if (leftX >= 1200) {
+                ele.style.left = -4800 + "px";    
+            }
         }
-
-
+        
     }
     move();
 }
