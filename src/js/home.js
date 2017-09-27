490c8bec76ec0c34d588be3e584c82a6973f7921
lcysnail <liuchengyingyx@gmail.com>
2017-09-27 11:43:58 +0800

    完善 轮播图

--- src/js/home.js
+++ src/js/home.js
@@ -41,33 +41,81 @@ var swiper_btn_left = Util.getElements('.swiper-btn-left')[0];
 var swiper_btn_right = Util.getElements('.swiper-btn-right')[0];
 var swiper_point_span = Util.getElements('.swiper-point-span');
 var timer1;
-
-Util.addEvent(swiper_btn_left, 'click', function(e) {
-    my_Animate(swiper_li, 'right', 1200, swiper_point_span);
-    
-});
-Util.addEvent(swiper_btn_right, 'click', function(e) {
-    my_Animate(swiper_li, 'left', 1200, swiper_point_span);
-});
-
-Util.addEvent(swiper_li, 'mouseenter', function (e) {
+var index = 1;
+Util.addEvent(swiper_btn_left, 'click', move_right);
+Util.addEvent(swiper_btn_right, 'click', move_left);
+Util.addEvent(swiper_li, 'mouseenter', function(e) {
     clearInterval(timer1);
 });
-
-Util.addEvent(swiper_li, 'mouseleave', function (e) {
+Util.addEvent(swiper_li, 'mouseleave', function(e) {
     timer1 = setInterval(function() {
-        my_Animate(swiper_li, 'left', 1200, swiper_point_span);
-    },5000);
+        my_Animate(swiper_li, 'left', 1200);
+        if (index > 4) {
+            index = 1;
+        } else {
+            index++;
+        }
+        pointMove(swiper_point_span, index);
+    }, 4000);
 });
 
 timer1 = setInterval(function() {
-    my_Animate(swiper_li, 'left', 1200, swiper_point_span);
-},5000);
+    my_Animate(swiper_li, 'left', 1200);
+    if (index > 4) {
+        index = 1;
+    } else {
+        index++;
+    }
+    pointMove(swiper_point_span, index);
+}, 4000);
+/**
+ * 向右滚动  事件触发的方法
+ */
+function move_right() {
+    my_Animate(swiper_li, 'right', 1200);
+    clearInterval(timer1);
+    if (index < 2) {
+        index = 5;
+    } else {
+        index--;
+    }
+    pointMove(swiper_point_span, index);
+}
+/**
+ * 向左滚动  事件触发的方法
+ */
+function move_left() {
+    my_Animate(swiper_li, 'left', 1200);
+    clearInterval(timer1);
+    if (index > 4) {
+        index = 1;
+    } else {
+        index++;
+    }
+    pointMove(swiper_point_span, index);
+}
 
+/**
+ * 轮播图圆点样式显示
+ * @param {*圆点元素[数组]} el 
+ * @param {*当前图片滚动的索引} index 
+ */
+function pointMove(el, index) {
+    for (var i = 0; i < el.length; i++) {
+        if (el[i].className.split(' ').length > 1) {
+            el[i].className = el[i].className.split(' ')[0];
+        }
+    }
+    el[index - 1].className += ' swiper-point-hover';
+}
+/**
+ * 轮播图 滚动  动画
+ * @param {*轮播图  图片的父元素} ele 
+ * @param {*滚动的方向} direction 
+ * @param {*一次滚动的距离} transitionX 
+ */
+function my_Animate(ele, direction, transitionX) {
 
-
-function my_Animate(ele, direction, transitionX, point_ani) {
-    
     var liftX = 0;
     var count = 0;
     if (direction == 'left') {
@@ -75,7 +123,7 @@ function my_Animate(ele, direction, transitionX, point_ani) {
     } else {
         leftX = ele.offsetLeft + 10;
     }
-    movePoint();
+
     function move() {
         var timer = setTimeout(function() {
             move();
@@ -94,21 +142,9 @@ function my_Animate(ele, direction, transitionX, point_ani) {
             ele.style.left = leftX + "px";
             leftX = leftX + 10;
             if (leftX >= 1200) {
-                ele.style.left = -4800 + "px";    
+                ele.style.left = -4800 + "px";
             }
-        }  
-    }
-    function movePoint() {
-        // var leftX = ele.offsetLeft;
-        point_ani[(Math.ceil(Math.abs(leftX) / 1200) - 1) == -1 ? 4 : Math.ceil(Math.abs(leftX) / 1200) - 1].className = point_ani[count].className.split(' ')[0];
-        // console.log(point_ani[count].className);
-        count = Math.ceil(Math.abs(leftX) / 1200) == 5?0:Math.ceil(Math.abs(leftX) / 1200);
-        // console.log(count);
-        // alert(1);
-        point_ani[count].className = point_ani[count].className + ' swiper-point-hover';
-        // console.log(point_ani[count].className);
+        }
     }
     move();
-    
 }
-
